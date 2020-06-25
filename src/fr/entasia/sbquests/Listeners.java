c6a06fa51afb5ef5c43c1a50633de19f1fb40850
iTrooz_ <team.mortelgames@gmail.com>
2020-06-25 10:34:15 +0200

    c + bo

--- src/fr/entasia/sbquests/Listeners.java
+++ src/fr/entasia/sbquests/Listeners.java
@@ -83,24 +83,30 @@ public class Listeners implements Listener {
 	@EventHandler
 	public static void onEntityKill(EntityDeathEvent e) {
 		Entity entity = e.getEntity();
-		if (e.getEntity().getKiller() == null) return;
 		Player killer = e.getEntity().getKiller();
-		if (Main.main.getConfig().getConfigurationSection("quests." + killer.getName()) != null) {
-			Quests current_quest = Quests.getByID(Main.main.getConfig().getInt("quests." + killer.getName() + ".id"));
+		if (killer==null) return;
+		BaseIsland is = BaseAPI.getIsland(CooManager.getIslandID(killer.getLocation()));
+		if(is==null||is.getMember(killer.getUniqueId())==null)return;
+		String id = is.isid.str();
+		if (Main.main.getConfig().getConfigurationSection("quests."+id) != null) {
+			Quests current_quest = Quests.getByID(Main.main.getConfig().getInt("quests."+id+".id"));
 			if(current_quest==null)return;
 			int i = 0;
 			for (QuestMob qm : current_quest.content.mobs) {
-				if (Main.main.getConfig().getInt("quests." + killer.getName() + ".mobs." + i) >= qm.number){i++;continue;}
+				if (Main.main.getConfig().getInt("quests." + id + ".mobs." + i) >= qm.number){
+					i++;
+					continue;
+				}
 				if (qm.type == entity.getType()) {
-					ConfigurationSection cs = Main.main.getConfig().getConfigurationSection("quests." + killer.getName() + ".mobs");
-					int mobs_number = cs.getInt("" + i);
+					ConfigurationSection cs = Main.main.getConfig().getConfigurationSection("quests." + id + ".mobs");
+					int mobs_number = cs.getInt(String.valueOf(i));
 					mobs_number++;
-					Main.main.getConfig().set("quests." + killer.getName() + ".mobs." + i, mobs_number);
+					Main.main.getConfig().set("quests." + id + ".mobs." + i, mobs_number);
 					if (mobs_number == qm.number) {
-						killer.sendMessage("§2Félicitation, vous avez tué §3" + qm.number + " " + qm.name + "§2, vous avez fini un objectif pour votre quête journaliere !");
+						killer.sendMessage("§2Félicitation, tu as tué §3" + qm.number + " " + qm.name + "§2, tu as fini un objectif pour ta quête d'île journaliere !");
 						killer.playSound(killer.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 3.5f, 1.1f);
 					} else {
-						killer.sendActionBar("§2Vous avez tué un(e) §3" + qm.name + "§2, plus que §3" + (qm.number - mobs_number) + "§2!");
+						killer.sendActionBar("§2Tu as tué un(e) §3" + qm.name + "§2, plus que §3" + (qm.number - mobs_number) + "§2!");
 						killer.playSound(killer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 3.5f, 1.1f);						}
 					break;
 				}
